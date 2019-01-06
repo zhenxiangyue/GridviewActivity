80921db4c0280e2a0f6ac670904e675a82c91c74
yue <zhenxiangyue@git>
2019-01-06 20:48:32 +0800

    提交测试

---
+++ app/src/main/java/gridview/study/com/gridviewactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package gridview.study.com.gridviewactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
